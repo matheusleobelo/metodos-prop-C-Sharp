fe15ab44ef4053e74d04ba5fbee4587b54192774
matheusleobelo <bipoladogamer@gmail.com>
2025-08-07 16:49:47 -0300

    Exemplo de propriedades e metodos em C-sharp

---
+++ Models/Pessoa.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        public string Nome
+        {
+            get => _nome.ToUpper();           
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentNullException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+        }
+        public int Idade {
+            get => _idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentNullException("Não existe idade negativa");
+                }
+                _idade = value;
+
+             } }
+        private string _nome;
+        private int _idade;
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá {Nome} que bom saber que você tem {Idade} anos");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Matheus";
+p1.Idade = 22;
+p1.Apresentar();
